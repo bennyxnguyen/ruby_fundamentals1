16801e9559f01d4f9b13e6a36fe931141560b21a
Benny <bennyxnguyen@gmail.com>
2016-05-31 14:54:04 -0400

    added exercise2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+meal_cost = 55
+tip_percent = 20
+
+tip_value = meal_cost * tip_percent/100
+puts tip_value
+
+num1 = "3"
+num2 = 2
+puts num1.to_i + num2
+
+puts "Ada Lovelace lived for #{45628 * 7839} years."
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
